1f15c71fa2bc2410226727c551e0e8dff2b02191
ngocsonqs <ngocson_jp@yahoo.co.jp>
2019-08-24 11:51:12 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Build RECIPE App with Reactjs
+
+---
+
+[TOC]
+
+---
+
+## 1. Yêu cầu:
+- Sử dụng webpack để create & build app
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode : 'development',
+    entry : {
+        bundle : './src/index.js'
+    },
+    output: {
+        filename : 'bundle.js',
+        path : path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: '/node_modules'
+            },
+            {
+                use: ['css-loader', 'style-loader'],
+                test: /\.css$/
+            }
+        ]
+    }
+};
