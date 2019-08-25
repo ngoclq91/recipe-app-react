c267cd625b9668f317aee0286a618afd940f6647
ngocsonqs <ngocson_jp@yahoo.co.jp>
2019-08-25 19:11:29 +0700

    setting public_path for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     output: {
         filename : 'bundle.js',
-        path : path.join(__dirname, 'dist')
+        path : path.join(__dirname, 'dist'),
+        publicPath: "/public/"
     },
     module: {
         rules: [
