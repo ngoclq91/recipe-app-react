bce300d5d6e8c295b86877c370e616cf1d8e23ed
ngocsonqs <ngocson_jp@yahoo.co.jp>
2019-08-26 22:28:03 +0700

    fix path for publicPath"

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path : path.join(__dirname, 'dist'),
         filename : '[name].js',
-        publicPath: "/public/"
+        publicPath: "./"
     },
     module: {
         rules: [
